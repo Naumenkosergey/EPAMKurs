92bd6f88330d9dd47d48fa0591b09fe9ec35eb79
naymc <naumenkosergeyns@gmail.com>
2019-11-04 16:08:05 +0300

    firstModul

--- src/modul1/modul1_1/Task5.java
+++ src/modul1/modul1_1/Task5.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Task5 {
 
     public static void main(String[] args) {
+        System.out.println("Hello");
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите число");
         int time = scanner.nextInt();
@@ -12,5 +13,8 @@ public class Task5 {
         int min = (time % 3600) / 60;
         int sek = (time % 3660) % 60 / 1;
         System.out.printf("для %d секунд прошло %d ч %d мин %d с", time, hours, min, sek);
+        System.out.printf("для %d секунд прошло %d ч %d мин %d с", time, hours, min, sek);
+        char c = 'h';
+
     }
 }
